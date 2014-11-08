f9a1d121ea468d84c467f8ebb70fed2436d87b44
Shuhao Wang <wshuhao@163.com>
2014-11-08 03:56:19 -0800

    set mongolab connection string

--- server.js
+++ server.js
@@ -7,46 +7,54 @@ var express = require("express"),
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
-function compile(str, path){
+
+function compile(str, path) {
     return stylus(str).set("filename", path);
 }
 
 app.set("views", __dirname + "/server/views");
-app.set("view engine","jade");
+app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(stylus.middleware({
-     src:__dirname + "/public",
-    compile:compile
+    src: __dirname + "/public",
+    compile: compile
 }));
 
 app.use(express.static(__dirname + "/public"));
 
-mongoose.connect("mongodb://localhost/MultiVision") 
+if (env == 'development') {
+    mongoose.connect("mongodb://localhost/MultiVision")
+} else {
+    mongoose.connect("mongodb://nodedev:multivision@ds051720.mongolab.com:51720/multivision");
+}
+
+
 var db = mongoose.connection;
-db.on('error',console.error.bind(console, 'connection error.')); 
-db.open('open', function callback(){
-	console.log("MultiVision db is opened");
-}) ;
+db.on('error', console.error.bind(console, 'connection error.'));
+db.open('open', function callback() {
+    console.log("MultiVision db is opened");
+});
 
-var messageSchema = mongoose.Schema({message:String}); 
-var Message = mongoose.model("Message", messageSchema); 
-var mongoMessage; 
-Message.findOne().exec(function(err, messageDoc){
-	mongoMessage = messageDoc.message;
-}); 
+var messageSchema = mongoose.Schema({
+    message: String
+});
+var Message = mongoose.model("Message", messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
 
 
-
-app.get("/partials/:partialPath", function(req,res){
+app.get("/partials/:partialPath", function(req, res) {
     res.render("partials/" + req.params.partialPath);
 });
 
-app.get("*", function(req, res){
+app.get("*", function(req, res) {
     res.render("index", {
-    	mongoMessage : mongoMessage
+        mongoMessage: mongoMessage
     });
 });
 
-var port = 3030 ;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log("Listening on port " + port + "....");
