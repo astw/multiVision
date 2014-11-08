7be177ecef0560a71c964015be845dddfe45b821
Shuhao Wang <wshuhao@163.com>
2014-11-08 03:03:48 -0800

    enable mongodb

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express"),
     stylus = require("stylus"),
     logger = require("morgan"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -20,12 +21,30 @@ app.use(stylus.middleware({
 
 app.use(express.static(__dirname + "/public"));
 
+mongoose.connect("mongodb://localhost/MultiVision") 
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error.')); 
+db.open('open', function callback(){
+	console.log("MultiVision db is opened");
+}) ;
+
+var messageSchema = mongoose.Schema({message:String}); 
+var Message = mongoose.model("Message", messageSchema); 
+var mongoMessage; 
+Message.findOne().exec(function(err, messageDoc){
+	mongoMessage = messageDoc.message;
+}); 
+
+
+
 app.get("/partials/:partialPath", function(req,res){
     res.render("partials/" + req.params.partialPath);
 });
 
 app.get("*", function(req, res){
-    res.render("index");
+    res.render("index", {
+    	mongoMessage : mongoMessage
+    });
 });
 
 var port = 3030 ;
